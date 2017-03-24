36b40b32618cadda28ee87d947aa50a915c543cb
Scott Donnelly <scott@donnel.ly>
2017-03-24 09:06:58 +0000

    Add integration with koa-jwt
    Adds an integration for compatibility with koa-jwt

---
+++ src/integrations/koa.js
@@ -0,0 +1,26 @@
+import { ArgumentError } from '../errors';
+import { JwksClient } from '../JwksClient';
+
+module.exports.koaJwtSecret = (options = {}) => {
+  const client = new JwksClient(options);
+
+  return function secretProvider({ alg, kid } = {}) {
+
+    return new Promise((resolve, reject) => {
+
+      // Only RS256 is supported.
+      if (alg !== 'RS256') {
+        return reject(new Error('Missing / invalid token algorithm'));
+      }
+
+      client.getSigningKey(kid, (err, key) => {
+        if (err) {
+          return reject(err);
+        }
+
+        // Provide the key.
+        resolve(key.publicKey || key.rsaPublicKey);
+      });
+    });
+  };
+};
