ca7f4620a66bd2389f9998d06e64a688c9b79378
Fabio Berta <fnberta@gmail.com>
2019-02-07 14:19:33 +0100

    proper format

--- index.d.ts
+++ index.d.ts
@@ -1,92 +1,96 @@
-declare module 'jwks-rsa' {
-  import * as ExpressJwt from 'express-jwt';
+import { SecretCallbackLong } from 'express-jwt';
 
-  function JwksRsa(options: JwksRsa.ClientOptions): JwksRsa.JwksClient;
+declare function JwksRsa(options: JwksRsa.ClientOptions): JwksRsa.JwksClient;
 
-  namespace JwksRsa {
-    class JwksClient {
-      constructor(options: ClientOptions);
+declare namespace JwksRsa {
+  class JwksClient {
+    constructor(options: ClientOptions);
 
-      getKeys(cb: (err: Error | null, keys: unknown) => void): void;
-      getSigningKeys(cb: (err: Error | null, keys: SigningKey[]) => void): void;
-      getSigningKey: (kid: string, cb: (err: Error | null, key: SigningKey) => void) => void;
-    }
+    getKeys(cb: (err: Error | null, keys: unknown) => void): void;
 
-    interface ClientOptions {
-      jwksUri: string;
-      rateLimit?: boolean;
-      cache?: boolean;
-      cacheMaxEntries?: number;
-      cacheMaxAge?: number;
-      jwksRequestsPerMinute?: number;
-      strictSsl?: boolean;
-    }
+    getSigningKeys(cb: (err: Error | null, keys: SigningKey[]) => void): void;
 
-    interface CertSigningKey {
-      kid: string;
-      nbf: string;
-      publicKey: string;
-    }
-
-    interface RsaSigningKey {
-      kid: string;
-      nbf: string;
-      rsaPublicKey: string;
-    }
-
-    type SigningKey = CertSigningKey | RsaSigningKey;
-
-    function expressJwtSecret(options: ExpressJwtOptions): ExpressJwt.SecretCallbackLong;
-
-    interface ExpressJwtOptions extends ClientOptions {
-      handleSigningKeyError?: (err: Error | null, cb: (err: Error | null) => void) => void;
-    }
-
-    function hapiJwt2Key(options: HapiJwtOptions): (decodedToken: DecodedToken, cb: HapiCallback) => void;
-
-    interface HapiJwtOptions extends ClientOptions {
-      handleSigningKeyError?: (err: Error | null, cb: HapiCallback) => void;
-    }
-
-    type HapiCallback = (err: Error | null, publicKey: string, signingKey: SigningKey) => void;
-
-    interface DecodedToken {
-      header: TokenHeader;
-    }
-
-    interface TokenHeader {
-      alg: string;
-      kid: string;
-    }
-
-    function hapiJwt2KeyAsync(options: HapiJwtOptions): (decodedToken: DecodedToken) => Promise<{ key: SigningKey }>;
-
-    function koaJwtSecret(options: KoaJwtOptions): (header: TokenHeader) => Promise<string>;
-
-    interface KoaJwtOptions extends ClientOptions {
-      handleSigningKeyError?(err: Error | null): Promise<void>;
-    }
-
-    class ArgumentError extends Error {
-      name: 'ArgumentError';
-      constructor(message: string);
-    }
-
-    class JwksError extends Error {
-      name: 'JwksError';
-      constructor(message: string);
-    }
-
-    class JwksRateLimitError extends Error {
-      name: 'JwksRateLimitError';
-      constructor(message: string);
-    }
-
-    class SigningKeyNotFoundError extends Error {
-      name: 'SigningKeyNotFoundError';
-      constructor(message: string);
-    }
+    getSigningKey: (kid: string, cb: (err: Error | null, key: SigningKey) => void) => void;
   }
 
-  export = JwksRsa;
+  interface ClientOptions {
+    jwksUri: string;
+    rateLimit?: boolean;
+    cache?: boolean;
+    cacheMaxEntries?: number;
+    cacheMaxAge?: number;
+    jwksRequestsPerMinute?: number;
+    strictSsl?: boolean;
+  }
+
+  interface CertSigningKey {
+    kid: string;
+    nbf: string;
+    publicKey: string;
+  }
+
+  interface RsaSigningKey {
+    kid: string;
+    nbf: string;
+    rsaPublicKey: string;
+  }
+
+  type SigningKey = CertSigningKey | RsaSigningKey;
+
+  function expressJwtSecret(options: ExpressJwtOptions): SecretCallbackLong;
+
+  interface ExpressJwtOptions extends ClientOptions {
+    handleSigningKeyError?: (err: Error | null, cb: (err: Error | null) => void) => void;
+  }
+
+  function hapiJwt2Key(options: HapiJwtOptions): (decodedToken: DecodedToken, cb: HapiCallback) => void;
+
+  interface HapiJwtOptions extends ClientOptions {
+    handleSigningKeyError?: (err: Error | null, cb: HapiCallback) => void;
+  }
+
+  type HapiCallback = (err: Error | null, publicKey: string, signingKey: SigningKey) => void;
+
+  interface DecodedToken {
+    header: TokenHeader;
+  }
+
+  interface TokenHeader {
+    alg: string;
+    kid: string;
+  }
+
+  function hapiJwt2KeyAsync(options: HapiJwtOptions): (decodedToken: DecodedToken) => Promise<{ key: SigningKey }>;
+
+  function koaJwtSecret(options: KoaJwtOptions): (header: TokenHeader) => Promise<string>;
+
+  interface KoaJwtOptions extends ClientOptions {
+    handleSigningKeyError?(err: Error | null): Promise<void>;
+  }
+
+  class ArgumentError extends Error {
+    name: 'ArgumentError';
+
+    constructor(message: string);
+  }
+
+  class JwksError extends Error {
+    name: 'JwksError';
+
+    constructor(message: string);
+  }
+
+  class JwksRateLimitError extends Error {
+    name: 'JwksRateLimitError';
+
+    constructor(message: string);
+  }
+
+  class SigningKeyNotFoundError extends Error {
+    name: 'SigningKeyNotFoundError';
+
+    constructor(message: string);
+  }
 }
+
+export = JwksRsa;
