f9f4d317987c35189b51b2e3b9f6b8df05909d77
Andries <andries.van.humbeeck@miaaguard.com>
2020-05-07 18:19:58 +0200

    Add documentation and order tests

--- tests/cache.tests.js
+++ tests/cache.tests.js
@@ -17,39 +17,45 @@ describe('JwksClient (cache)', () => {
   });
 
   describe('#getSigningKey', () => {
-    describe('with lru-cache', () => {
-      describe('should cache requests per kid', () => {
-        let client;
+    describe('should cache requests per kid', () => {
+      let client;
 
-        before((done) => {
-          nock(jwksHost)
-            .get('/.well-known/jwks.json')
-            .reply(200, x5cSingle);
+      before((done) => {
+        nock(jwksHost)
+          .get('/.well-known/jwks.json')
+          .reply(200, x5cSingle);
 
-          client = new JwksClient({
-            cache: true,
-            jwksUri: `${jwksHost}/.well-known/jwks.json`
-          });
-
-          // Cache the Key
-          client.getSigningKey('NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA', (err, key) => {
-            expect(key.kid).to.equal('NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA');
-
-            // Stop the JWKS server
-            nock.cleanAll();
-            done();
-          });
+        client = new JwksClient({
+          cache: true,
+          jwksUri: `${jwksHost}/.well-known/jwks.json`
         });
 
-        it('should ignore the cache when the KID isnt cached and make a requst', (done) => {
-          client.getSigningKey('12345', (err) => {
-            expect(err).not.to.be.null;
-            expect(err.code).to.equal('ENOTFOUND');
-            done();
+        // Cache the Key
+        client.getSigningKey('NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA', (err, key) => {
+          expect(key.kid).to.equal('NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA');
 
-          });
+          // Stop the JWKS server
+          nock.cleanAll();
+          done();
         });
       });
+
+      it('should ignore the cache when the KID isnt cached and make a requst', (done) => {
+        client.getSigningKey('12345', (err) => {
+          expect(err).not.to.be.null;
+          expect(err.code).to.equal('ENOTFOUND');
+          done();
+
+        });
+      });
+
+      it('should fetch the key from the cache', (done) => {
+        client.getSigningKey('NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA', (err, key) => {
+          expect(key.kid).to.equal('NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA');
+          done();
+        });
+      });
+
     });
 
     describe('with local file cache', () => {
@@ -101,13 +107,6 @@ describe('JwksClient (cache)', () => {
             expect(err.code).to.equal('ENOTFOUND');
             done();
           });
-        });
-      });
-
-      it('should fetch the key from the cache', (done) => {
-        client.getSigningKey('NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA', (err, key) => {
-          expect(key.kid).to.equal('NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA');
-          done();
         });
       });
 
