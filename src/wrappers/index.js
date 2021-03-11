928c5d4911297e87a9b88a0bea2f532bb6897750
David <david.patrick@auth0.com>
2021-03-11 14:45:59 -0800

    Callback backwards compatibility
    To have better backwards compatibility for v2, add callback support on getSigningKey

--- src/wrappers/index.js
+++ src/wrappers/index.js
@@ -2,5 +2,6 @@ module.exports = {
   request: require('./request').default,
   cacheSigningKey: require('./cache').default,
   rateLimitSigningKey: require('./rateLimit').default,
-  getKeysInterceptor: require('./interceptor').default
+  getKeysInterceptor: require('./interceptor').default,
+  callbackSupport: require('./callbackSupport').default
 };
