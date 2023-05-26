f26f363af8930f86c85ba0659ba6337fcc94407b
Nat Welch <nat@natwelch.com>
2023-05-26 15:20:14 +0000

    fix: jose

--- tests/mocks/jwks.js
+++ tests/mocks/jwks.js
@@ -1,12 +1,12 @@
 const nock = require('nock');
-const jose2 = require('jose2');
+const jose2 = require('jose');
 
 function jwksEndpoint(host, certs) {
   return nock(host)
     .get('/.well-known/jwks.json')
     .reply(200, {
       keys: certs.map(cert => {
-        const parsed = jose2.JWK.asKey(cert.pub).toJWK();
+        const parsed = jose.JWK.asKey(cert.pub).toJWK();
         return {
           ...parsed,
           use: 'sig',
