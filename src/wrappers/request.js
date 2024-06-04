a29e3cce5de009302159531a185bb147647f705a
Karthikeyan <k@kaos.agency>
2024-06-04 08:28:53 +0530

    timeout support not present in fetch

--- src/wrappers/request.js
+++ src/wrappers/request.js
@@ -1,52 +1,27 @@
-const http = require('http');
-const https = require('https');
-const urlUtil = require('url');
+const urlUtil = require("url");
+const fetch = require("node-fetch"); // Ensure to install node-fetch
 
-module.exports.default =  (options) => {
+module.exports.default = async (options) => {
   if (options.fetcher) {
     return options.fetcher(options.uri);
   }
 
-  return new Promise((resolve, reject) => {
-    const {
-      hostname,
-      path,
-      port,
-      protocol
-    } = urlUtil.parse(options.uri);
+  const { hostname, path, port, protocol } = urlUtil.parse(options.uri);
+  const url = `${protocol}//${hostname}${port ? `:${port}` : ""}${path}`;
 
-    const requestOptions = {
-      hostname,
-      path,
-      port,
-      method: 'GET',
-      ...(options.headers && { headers: { ...options.headers } }),
-      ...(options.timeout && { timeout: options.timeout }),
-      ...(options.agent && { agent: options.agent })
-    };
+  const requestOptions = {
+    method: "GET",
+    headers: options.headers || {},
+    agent: options.agent || undefined,
+  };
 
-    const httpRequestLib = protocol === 'https:' ? https : http;
-    const httpRequest = httpRequestLib.request(requestOptions, (res) => {
-      let rawData = '';
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => { rawData += chunk; });
-      res.on('end', () => {
-        if (res.statusCode < 200 || res.statusCode >= 300) {
-          const errorMsg = res.body && (res.body.message || res.body) || res.statusMessage || `Http Error ${res.statusCode}`;
-          reject({ errorMsg });
-        } else {
-          try {
-            resolve(rawData && JSON.parse(rawData));
-          } catch (error) {
-            reject(error);
-          }
-        }
-      });
-    });
+  const response = await fetch(url, requestOptions);
+  const rawData = await response.text();
 
-    httpRequest
-      .on('timeout', () => httpRequest.destroy())
-      .on('error', (e) => reject(e))
-      .end();
-  });
+  if (!response.ok) {
+    const errorMsg = response.statusText || `Http Error ${response.status}`;
+    throw { errorMsg };
+  }
+
+  return rawData ? JSON.parse(rawData) : {};
 };
